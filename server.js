5b130641850ef040b3911983e2a47e74ab239407
shadowmoonezios <agushifelde@hotmail.com>
2025-09-27 15:15:55 +0300

    Fix typo in console log message for clarity

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 const mongoDBURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/essenbestellen';
 mongoose.connect(mongoDBURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('Datenbank verbunden!'))
+.then(() => console.log('Datenbank erfolgreich verbunden!'))
 .catch(err => console.error('Datenbankverbindung fehlgeschlagen:', err));
 
 app.listen(PORT, () => {
